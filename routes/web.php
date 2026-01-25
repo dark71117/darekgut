da27f7bdd2e90f31554248c9009767314b0f4cdc
dark71117 <darek@darekgut.net>
2026-01-25 18:53:05 +0100

    zmiany www

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\ProjectController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::resource('clients', ClientController::class);
+Route::resource('projects', ProjectController::class);
